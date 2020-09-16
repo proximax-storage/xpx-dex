576393d4469ff727a710d4b4873f9c2ef41579dd
devyin7 <devyin7@gmail.com>
2020-09-16 14:41:14 -0500

    add validate

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,7 +9,7 @@
       color="leveint"
     >
       <v-toolbar-title>
-        <img :src="require('@/assets/img/logo-dex-color.svg')" alt="logo" height="35" />
+        <img :src="require('@/assets/img/logo-dex-blanco.svg')" alt="logo" height="35" />
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- <v-btn icon color="primary" @click="activity('myWallet', 0)">
