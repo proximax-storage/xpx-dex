8272da9109a1cd341c8bf74b24396d02970a1bc4
dionisux <dionisus.78@gmail.com>
2020-10-08 18:17:39 -0400

    mod deploy/xpx-deploy.sh

--- deploy/xpx-deploy.sh
+++ deploy/xpx-deploy.sh
@@ -5,7 +5,6 @@ set -e
 
 # actualizar
 npm i --force ; npm audit fix ; npm update
-git clone git@github.com:devyin7/tsjs-sirius-suite-wallet-library.git
-cp -rv tsjs-sirius-suite-wallet-library/ node_modules/
+cp -rv deploy/tsjs-sirius-suite-wallet-library/ node_modules/
 # build
 npm run build
