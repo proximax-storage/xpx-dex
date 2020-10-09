ac90f74396a046aff9cbd24952b29f507953e7cd
devyin7 <devyin7@gmail.com>
2020-10-09 08:14:52 -0500

    add validate

--- src/core/components/nodes/NodeStatus.vue
+++ src/core/components/nodes/NodeStatus.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="12 pr-0 ">
+      <v-col cols="6 pr-0 ">
         <!-- <template v-if="currentNode"> -->
         <div class="text-center pt-1 d-flex align-center">
           <span class="caption" :title="currentNode">
@@ -33,13 +33,26 @@
         </div>
         <!-- </template> -->
       </v-col>
+      <template v-if="nodeStatus === serverStatus.NODE_OFF">
+        <v-col cols="6 pr-0">
+          <div class="text-center pt-1 d-flex align-center">
+            <v-btn icon small @click="reconnect()">
+                  <span class="caption" title="Reconnect node">
+
+              <v-icon>mdi-cached</v-icon>
+                </span>
+            </v-btn>
+          </div>
+        </v-col>
+      </template>
     </v-row>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 import { TypeStatusNode } from 'tsjs-sirius-suite-wallet-library/dist/model/nodes/blockchain/Websocket'
+import { NodeService } from '@/services/blockchain/node-service'
 export default {
   data () {
     return {
@@ -48,12 +61,19 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('nodesStoreNew', ['currentNode', 'nodeStatus'])
+    ...mapGetters('nodesStoreNew', ['currentNode', 'nodeStatus', 'currentNode'])
     // NODE_OFF = 0,
     //   NODE_CONNECTING = 1,
     //   NODE_ACTIVE = 2,
     //   NODE_STUCK = 3,
     //   NODE_SYNCHRONIZING = 4
+  },
+  methods: {
+    ...mapMutations('nodesStoreNew', ['RECONNECT']),
+    reconnect (node = this.currentNode) {
+      NodeService.connect(node)
+      this.RECONNECT(true)
+    }
   }
 }
 </script>
