4d5c4c82ea399e242053ca8a7a910eae90265c7a
Jeffersson Espinoza <devyin7@gmail.com>
2021-01-12 11:59:16 -0500

    add test

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,16 +1,28 @@
 <template>
-  <v-footer :inset="footer.inset" app color="leveint"  dense class="font-weight-light caption">
-    <v-col class="text-center ma-0 pa-0" cols="12"
-      ><span class="px-4">Copyright&copy; {{new Date().getFullYear()}} ProximaX&reg;. All Rights Reserved.</span>
+  <v-footer
+    :inset="footer.inset"
+    app
+    color="leveint"
+    dense
+    class="font-weight-light caption"
+  >
+    <v-col
+      class="text-center ma-0 pa-0"
+      cols="12"
+    ><span class="px-4">Copyright&copy;{{new Date().getFullYear()}} ProximaX&reg;. All Rights Reserved. v{{version}}</span>
     </v-col>
   </v-footer>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data: () => ({
     footer: {
       inset: true
     }
-  })
+  }),
+  computed: {
+    ...mapGetters(['version'])
+  }
 }
 </script>
