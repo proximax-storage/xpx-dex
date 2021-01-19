caffac4c816319f947fe8d7633118e9e0ab6f382
Jeffersson Espinoza <devyin7@gmail.com>
2021-01-19 14:46:50 -0500

    add new viws

--- src/views/signup/SelectTypeSignup.vue
+++ src/views/signup/SelectTypeSignup.vue
@@ -2,15 +2,35 @@
   <v-container class="fill-height">
     <v-container>
       <v-row class="d-flex justify-center align-center mb-8 mt-1">
-        <img :src="require('@/assets/img/logo-dex-blanco.svg')" alt="logo" height="70" />
+        <img
+          :src="require('@/assets/img/logo-dex-blanco.svg')"
+          alt="logo"
+          height="70"
+        />
       </v-row>
       <v-row class="d-flex justify-center align-center">
-        <v-col xl="6" lg="6" md="7" sm="8" xs="12">
-          <v-card elevation="1" class="elevation-2 leve">
+        <v-col
+          xl="6"
+          lg="6"
+          md="7"
+          sm="8"
+          xs="12"
+        >
+          <v-card
+            elevation="1"
+            class="elevation-2 leve"
+          >
             <v-card-text>
               <v-row>
                 <template v-for="(item, key) of items">
-                  <v-col :key="key" cols="10" sm="6" md="6" lg="6" class="text-center mx-auto">
+                  <v-col
+                    :key="key"
+                    cols="12"
+                    sm="4"
+                    md="4"
+                    lg="4"
+                    class="text-center mx-auto"
+                  >
                     <div class="d-flex justify-center pb-2 pt-8">
                       <v-img
                         :src="require(`@/assets/img/${theme}/${item.icon}`)"
@@ -35,10 +55,17 @@
               </v-row>
             </v-card-text>
             <v-card-actions class="white pa-0">
-              <v-row class =" ml-2">
-                <v-col class="d-flex justify-start darkness--text">  I already have an account</v-col>
+              <v-row class=" ml-2">
+                <v-col class="d-flex justify-start darkness--text"> I already have an account</v-col>
                 <v-col class="d-flex justify-end">
-                  <v-btn router text small color="primary" to="/" class="font-weight-bold pt-1 pb-1 mr-4">Sign In</v-btn>
+                  <v-btn
+                    router
+                    text
+                    small
+                    color="primary"
+                    to="/"
+                    class="font-weight-bold pt-1 pb-1 mr-4"
+                  >Sign In</v-btn>
                 </v-col>
               </v-row>
             </v-card-actions>
@@ -46,7 +73,10 @@
         </v-col>
       </v-row>
       <v-row class="d-flex justify-center align-center ma-4">
-        <router-link class="font-italic font-weight-medium" to="/">Go to home page</router-link>
+        <router-link
+          class="font-italic font-weight-medium"
+          to="/"
+        >Go to home page</router-link>
       </v-row>
     </v-container>
   </v-container>
@@ -70,6 +100,12 @@ export default {
           icon: 'icon-private-key.svg',
           route: '/create-from-privateKey'
         }
+        // {
+        //   title: 'From a Private Key',
+        //   nameButton: 'Create',
+        //   icon: 'icon-private-key.svg',
+        //   route: '/create-from-privateKey'
+        // }
       ]
     }
   }
